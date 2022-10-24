e2b6d954345409adc858bfe3372ab47349532f2b
bhagavath <lordcodeking@gmail.com>
2022-10-24 14:55:10 +0530

    initial

---
+++ src/main/java/com/blaps/blaps/BlapsApplication.java
@@ -0,0 +1,13 @@
+package com.blaps.blaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blaps/blaps/BlapsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blaps.blaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
